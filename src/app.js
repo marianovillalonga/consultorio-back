ec7164470566927c126181e6ace355d589def895
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2026-01-08 17:38:39 -0300

    Arreglo en tamaño estudios

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 app.use(cors(corsOptions))
 
 app.use(helmet())
-app.use(express.json({ limit: '1mb' }))
+app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
 
 app.use((req, res, next) => req.method === 'OPTIONS' ? next() : csrfProtect(req, res, next))
