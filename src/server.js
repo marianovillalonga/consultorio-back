6c1cbc288491489c7f7f622d69d29dd8eb60dc57
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2026-04-13 17:43:22 -0300

    Reparo

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-﻿import app from './app.js'
-import { env } from './config/env.js'
-import { sequelize } from './db/sequelize.js'
-import './models/index.js'
-
 const start = async () => {
     try {
+        const [{ default: app }, { env }, { sequelize }] = await Promise.all([
+            import('./app.js'),
+            import('./config/env.js'),
+            import('./db/sequelize.js')
+        ])
+
+        await import('./models/index.js')
         await sequelize.authenticate()
-        app.listen(env.port, () => console.log(`🚀 API en puerto ${env.port}`))
+        app.listen(env.port, () => console.log(`API en puerto ${env.port}`))
     } catch (err) {
         console.error('[startup] Error al iniciar', err)
         process.exit(1)
@@ -14,4 +16,3 @@ const start = async () => {
 }
 
 start()
-
